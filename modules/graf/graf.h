3a18abe0604032857b8d3f347e0fc51dcf717780
Aland Mulia Pratama <13522124@std.stei.itb.ac.id>
2023-10-30 19:43:51 +0700

    add file adt graf

---
+++ modules/graf/graf.h
@@ -0,0 +1,14 @@
+/* ********** Definisi Graf dengan representasi matriks adjency ********** */
+
+#ifndef GRAF_H
+#define GRAF_H
+
+#include "../boolean.h"
+
+/*Ukuran Maksimum sesuai jumlah user*/
+#define ROW_CAP 20
+#define COL_CAP 20
+
+
+
+#endif
