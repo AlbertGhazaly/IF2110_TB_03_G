6576bfcce76b9d071b937d3ce1af658d615b612f
Jimly Nur Arif <132258178+jimlynurarif@users.noreply.github.com>
2023-10-30 19:45:53 +0700

    Update DrafKicauan.h

--- main/DrafKicauan/DrafKicauan.h
+++ main/DrafKicauan/DrafKicauan.h
@@ -1,1 +1,26 @@
 #include "../../modules/adt.h"
+
+// Struct untuk menyimpan draf
+struct Draft
+{
+  int id;
+  char text[280]; // sesuai spek halaman 22
+  char author[256];
+  char datetime[30];
+};
+
+// Fungsi untuk menghitung jumlah baris dalam file
+int HitungJumlahBaris(const char *namaFile);
+
+// Fungsi untuk mengedit baris tertentu dari file
+void editConfigFile(const char *filename, int line_number, const char *new_text);
+
+// Fungsi untuk membuat draf baru
+void BUAT_DRAF(const char *namaFile);
+
+// Fungsi untuk melihat semua draf
+void LIHAT_DRAF(const char *namaFile);
+
+
+
+  
