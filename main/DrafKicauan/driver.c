59dd88b505a7b29172517188024720bb719e2c9c
Jimly Nur Arif <132258178+jimlynurarif@users.noreply.github.com>
2023-10-31 09:25:44 +0700

    Create driver.c

---
+++ main/DrafKicauan/driver.c
@@ -0,0 +1,32 @@
+/*file driver.c*/
+#include "DrafKicauan.h"
+#include "DrafKicauan.c"
+
+int main()
+{
+  Word kata;
+  boolean sama;
+
+  printf("BUAT_DRAF atau LIHAT_DRAF? ");
+  STARTWORD();
+
+  // Memasukkan kata yang diakuisisi ke dalam variabel 'kata'
+  CopyWordTo(&kata, currentWord);
+
+  // Membandingkan kata dengan kata referensi
+  sama = WordEqual(kata, stringToWord("BUAT_DRAF", 9));
+  // input harus diakhiri titik koma ";"
+
+  while (getchar() != '\n');
+
+  if (sama)
+  {
+    BUAT_DRAF("draf.config");
+  }
+  else
+  {
+    LIHAT_DRAF("draf.config");
+  }
+
+  return 0;
+}
