ad1802ffdfc7306988d644b0f770580a7abf60ff
Albert Ghazaly <albert260306@gmail.com>
2023-11-02 14:26:48 +0700

    Update: SimpanMuat.c (base)

--- main/SimpanMuat/SimpanMuat.c
+++ main/SimpanMuat/SimpanMuat.c
@@ -1,1 +1,43 @@
 #include "SimpanMuat.h"
+
+void muat(char floderName[]){
+    for (int i=0;i<5;i++){
+        if (i==0){ // User config
+
+        }
+        else if (i==1){ // Tweet config
+
+        } 
+        else if (i==2){ // Reply config
+
+        }
+        else if (i==3){ // Draft config
+
+        }
+        else if (i==4){ //Utas config
+
+        }
+
+    }
+}
+
+void simpan(char folderName[]){
+    for (int i= 0;i<5;i++){
+        if (i==0){ // User config
+
+        }
+        else if (i==1){ // Tweet config
+
+        } 
+        else if (i==2){ // Reply config
+
+        }
+        else if (i==3){ // Draft config
+
+        }
+        else if (i==4){ //Utas config
+
+        }
+  
+    }
+}
--- main/SimpanMuat/SimpanMuat.h
+++ main/SimpanMuat/SimpanMuat.h
@@ -1,1 +1,17 @@
+#ifndef SimpanMuat_H
+#define SimpanMuat_H
+
 #include "../../modules/adt.h"
+void loadUserCnfg();
+void loadTweetCnfg();
+void loadReplyCnfg();
+void loadDraftCnfg();
+void loadUtasCnfg();
+
+void saveUserCnfg();
+void saveTweetCnfg();
+void saveReplyCnfg();
+void saveDraftCnfg();
+void saveUtasCnfg();
+
+#endif
