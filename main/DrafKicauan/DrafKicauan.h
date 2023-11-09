4e88dc27157dc99509753a6c193a3934296ff316
Jimly Nur Arif <132258178+jimlynurarif@users.noreply.github.com>
2023-11-09 07:22:39 +0700

    Update DrafKicauan.h

--- main/DrafKicauan/DrafKicauan.h
+++ main/DrafKicauan/DrafKicauan.h
@@ -2,8 +2,12 @@
 #define DrafKicauan_H
 
 #include "../../modules/adt.h"
+#include "../Pengguna/Pengguna.h"
 
+Stack* createSTackDrafPerUser(Stack S);
 
+void buatdraf(Stack *draf, int userID, AccountList *list);
 
+void lihatdraf(Stack *draf, int userID, AccountList *list);
 
 #endif
