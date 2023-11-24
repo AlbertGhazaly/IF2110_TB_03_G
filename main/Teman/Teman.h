99d83e817f59f8eea49f020b81bd687c3c04d2db
Aland Mulia Pratama <13522124@std.stei.itb.ac.id>
2023-11-24 08:49:17 +0700

    style: add comment

--- main/Teman/Teman.h
+++ main/Teman/Teman.h
@@ -5,7 +5,15 @@
 
 
 void daftarteman(boolean login, Account akunlogin, AccountList* listakun, Graf teman);
+/* Menampilkan daftar teman
+    I.S. : login terdefinisi, akunlogin terdefinisi, listakun terdefinisi, teman terdefinisi
+    F.S. : ditampilkan daftar teman
+*/
 
 void hapusteman(boolean login, Account akunlogin, AccountList* listakun, Graf* teman);
+/* Menghapus teman
+    I.S. : login terdefinisi, akunlogin terdefinisi, listakun terdefinisi, teman terdefinisi
+    F.S. : teman dihapus
+*/
 
 #endif
